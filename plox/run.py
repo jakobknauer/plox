76849665f8c94521dc6d2183e04fd0844ad7e05f
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2023-12-16 15:18:18 +0100

    Refactor run.py

--- plox/run.py
+++ plox/run.py
@@ -9,88 +9,77 @@ from plox.scanning import Scanner
 from plox.tokens import Token, TokenType
 
 
-had_error = False
-had_interpreter_error = False
-
-
-def error(line: int, message: str):
-    report(line, "", message)
-
-
-def error2(token: Token, message: str):
-    if token.type == TokenType.EOF:
-        report(token.line, " at end", message)
-    else:
-        report(token.line, f" at '{token.lexeme}'", message)
-
-
-def interpreter_error(ie: InterpreterError):
-    global had_interpreter_error
-    print(f"{ie.message}\n[line {ie.token.line}]")
-    had_interpreter_error = True
-
-
-def report(line: int, where: str, message: str):
-    global had_error
-    print(f"[line {line}] Error{where}: {message}")
-    had_error = True
-
-
-interpreter = Interpreter(error_callback=interpreter_error)
-
-
 def main():
     if len(sys.argv) > 2:
         print("Usage: plox [script]")
         sys.exit(64)
     elif len(sys.argv) == 2:
-        run_file(sys.argv[1])
+        Application().run_file(sys.argv[1])
     else:
-        run_prompt()
+        Application().run_prompt()
 
 
-def run_file(path: str):
-    with open(path) as file:
-        source = file.read()
+class Application:
+    def __init__(self):
+        self._had_error = False
+        self._had_interpreter_error = False
+        self._interpreter = Interpreter(error_callback=self._interpreter_error)
 
-    run(source)
+    def run_file(self, path: str):
+        with open(path) as file:
+            source = file.read()
 
-    if had_error:
-        sys.exit(65)
-    if had_interpreter_error:
-        sys.exit(70)
+        self.run(source)
 
+        if self._had_error:
+            sys.exit(65)
+        if self._had_interpreter_error:
+            sys.exit(70)
 
-def run_prompt():
-    global had_error
+    def run_prompt(self):
+        while True:
+            line = input("> ")
 
-    while True:
-        line = input("> ")
+            if not line:
+                break
 
-        if not line:
-            break
+            self.run(line)
+            self._had_error = False
 
-        run(line)
-        had_error = False
+    def run(self, source: str) -> None:
+        scanner = Scanner(source, self._error)
+        tokens = scanner.scan_tokens()
 
+        parser = Parser(tokens, self._error2)
+        statements = parser.parse()
 
-def run(source: str):
-    scanner = Scanner(source, error)
-    tokens = scanner.scan_tokens()
+        if self._had_error:
+            return
 
-    parser = Parser(tokens, error2)
-    statements = parser.parse()
+        resolver = Resolver(self._interpreter, self._error2)
+        resolver.resolve(statements)
 
-    if had_error:
-        return
+        if self._had_error:
+            return
 
-    resolver = Resolver(interpreter, error2)
-    resolver.resolve(statements)
+        self._interpreter.interpret(statements)
 
-    if had_error:
-        return
+    def _error(self, line: int, message: str):
+        self._report(line, "", message)
 
-    interpreter.interpret(statements)
+    def _error2(self, token: Token, message: str):
+        if token.type == TokenType.EOF:
+            self._report(token.line, " at end", message)
+        else:
+            self._report(token.line, f" at '{token.lexeme}'", message)
+
+    def _interpreter_error(self, ie: InterpreterError):
+        print(f"{ie.message}\n[line {ie.token.line}]")
+        self._had_interpreter_error = True
+
+    def _report(self, line: int, where: str, message: str):
+        print(f"[line {line}] Error{where}: {message}")
+        self._had_error = True
 
 
 if __name__ == "__main__":
