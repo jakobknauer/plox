8f38f30cf3050e34e7afec581d8c73dd962293bb
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2023-12-16 15:18:18 +0100

    Refine AstPrinter

--- plox/util.py
+++ plox/util.py
@@ -1,24 +1,24 @@
+# pylint: disable=function-redefined
+# mypy: disable-error-code="no-redef"
+
 from plox import expression
 from plox.visitor import visitor
 
 
-class AstPrinter:
-    @visitor(expression.Expr)
-    def print(self, expr: expression.Expr) -> str:
-        pass
 
+class AstPrinter:
     @visitor(expression.Binary)
-    def print_binary(self, binary: expression.Binary) -> str:
+    def print(self, binary: expression.Binary) -> str:
         return f"{binary.operator.lexeme} {self.print(binary.left)} {self.print(binary.right)}"
 
     @visitor(expression.Grouping)
-    def print_grouping(self, grouping: expression.Grouping) -> str:
+    def print(self, grouping: expression.Grouping) -> str:
         return f"({self.print(grouping.expression)})"
 
     @visitor(expression.Literal)
-    def print_literal(self, literal: expression.Literal) -> str:
+    def print(self, literal: expression.Literal) -> str:
         return f"{literal.value}"
 
     @visitor(expression.Unary)
-    def print_unary(self, unary: expression.Unary) -> str:
+    def print(self, unary: expression.Unary) -> str:
         return f"{unary.operator.lexeme} {self.print(unary.right)}"
